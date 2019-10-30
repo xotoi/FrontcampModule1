ac0551255d25178ce6de83ef0e5c41e84cb32a1a
ysakalou <xotoi@yandex.ru>
2019-10-30 14:07:23 +0300

    resolve module comments

---
+++ src/clickHandlers/handlers.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+export default function sourceSelectHandler(sourceId, service){
+    service.getNewsFromSource(sourceId).then(result => {
+        $('.card').remove();
+
+        result.articles.map((article) => {
+            $('body').append(
+                $('<div>', {
+                'class': 'card',
+                'style': 'width: 18rem;'
+            }).append(
+                $('<img>', {
+                    'class': 'card-img-top',
+                    'src': article.urlToImage
+                }),
+                $('<div>', {
+                    'class': 'card-body',
+                }).append(
+                    $('<p>', {
+                        'class': 'card-text'
+                    }).text(article.content)
+                    )
+                    
+                )
+            );
+        });
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,40 +1,19 @@
 import $ from 'jquery';
-import newsApiService from './services/newsApiService'
+import newsApiService from './services/newsApiService';
+import 'bootstrap';
 
 const service = new newsApiService();
 service.getNewsSources().then(result => {
     for(var i =0; i < 11; i++){
         const source = result.sources[i];
         const selectorElement = $(`<button id="${source.id}" class="dropdown-item" type="button">${source.name}</a>`);
-        selectorElement.click(function() {sourceSelectHandler(source.id)});
+        selectorElement.click(function() {onSelectorClick(source.id, service)});
         $('#sourceSelector').append(selectorElement);
     };
 });
 
-function sourceSelectHandler(sourceId){
-    service.getNewsFromSource(sourceId).then(result => {
-        $('.card').remove();
-
-        result.articles.map((article) => {
-            $('body').append(
-                $('<div>', {
-                'class': 'card',
-                'style': 'width: 18rem;'
-            }).append(
-                $('<img>', {
-                    'class': 'card-img-top',
-                    'src': article.urlToImage
-                }),
-                $('<div>', {
-                    'class': 'card-body',
-                }).append(
-                    $('<p>', {
-                        'class': 'card-text'
-                    }).text(article.content)
-                    )
-                    
-                )
-            );
-        });
-    });
-};
+async function onSelectorClick(id, service){
+    import('./clickHandlers/handlers').then((module) => {
+        module.default(id, service)
+    })
+}
