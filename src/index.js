4216e849c8162fcfa37f909ccefb639f4efb11c3
ysakalou <xotoi@yandex.ru>
2019-10-30 14:55:37 +0300

    add spread operator

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import 'bootstrap';
 
 const service = new newsApiService();
 service.getNewsSources().then(result => {
+    let sources = [];
     for(var i =0; i < 11; i++){
         const { id, name } = result.sources[i];
         const selectorElement = $(`<button id="${id}" class="dropdown-item" type="button">${name}</a>`);
         selectorElement.click(function() {onSelectorClick(id, service)});
-        $('#sourceSelector').append(selectorElement);
+        sources.push(selectorElement);
     };
+    $('#sourceSelector').append([...sources]);
 });
 
 async function onSelectorClick(id, service){
